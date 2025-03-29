e352e3a574795def82e41735b33441ce65fc95bc
Marek Joukl <jouklmarek@seznam.cz>
2025-03-29 18:33:03 +0100

    add initial travel list application with components, assets, and README

---
+++ travel-list/README.md
@@ -0,0 +1,11 @@
+# Simple travel list planner
+
+![Screenshot](./public/screenshot.png)
+
+## How to run
+
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm start`
+
+> This project was done as a part of [React Course](https://www.udemy.com/course/the-ultimate-react-course/)
---
+++ travel-list/src/components/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+import Form from "./Form.js";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleDeleteAllItems(num) {
+    if (num) {
+      const confirmed = window.confirm("Are you sure?");
+      if (confirmed) setItems([]);
+    }
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onDeleteAllItems={handleDeleteAllItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
