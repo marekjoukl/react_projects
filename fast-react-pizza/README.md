60ce48dd2ff8578e1ce9242da7a39a2b705ee19e
Marek Joukl <jouklmarek@seznam.cz>
2025-03-29 18:40:13 +0100

    add initial Fast React Pizza application with components, configuration, and README

---
+++ fast-react-pizza/README.md
@@ -0,0 +1,11 @@
+# Real-World Pizza Delivery App
+
+![Screenshot](./public/screenshot.png)
+
+## How to run
+
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm run dev`
+
+> This project was done as a part of [React Course](https://www.udemy.com/course/the-ultimate-react-course/)
---
+++ fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -0,0 +1,64 @@
+import { formatCurrency } from "../../utils/helpers";
+import Button from "../../ui/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
+
+function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      unitPrice,
+      quantity: 1,
+      totalPrice: unitPrice,
+    };
+    dispatch(addItem(newItem));
+  }
+
+  return (
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
+      <div className="flex flex-grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
+          {currentQuantity > 0 && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                currentQuantity={currentQuantity}
+                pizzaId={id}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
+          {!soldOut && !currentQuantity && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default MenuItem;
